958a0601153717720b5888d4c32ba871428d910c
q13x <no@no.com>
2022-12-20 11:28:37 -0800

    some refactoring

--- meta.ts
+++
@@ -1,3 +0,0 @@
-export const BRANDING: Readonly<string> = Object.freeze("EaglerXProxy")
-export const VERSION: Readonly<string> = "1.0.0"
-export const NETWORK_VERSION: Readonly<string> = Object.freeze(BRANDING + "/" + VERSION)
