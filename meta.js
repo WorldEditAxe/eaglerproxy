9965b1370e18bbd3093d785e26e8fb81b95e6073
q13x <no@no.com>
2022-12-20 11:20:28 -0800

    Remove precompiled .js files

--- meta.js
+++
@@ -1,3 +0,0 @@
-export const BRANDING = Object.freeze("EaglerXProxy");
-export const VERSION = "1.0.0";
-export const NETWORK_VERSION = Object.freeze(BRANDING + "/" + VERSION);
--- types.js
+++
@@ -1,6 +0,0 @@
-export var State;
-(function (State) {
-    State[State["PRE_HANDSHAKE"] = 0] = "PRE_HANDSHAKE";
-    State[State["POST_HANDSHAKE"] = 1] = "POST_HANDSHAKE";
-    State[State["DISCONNECTED"] = 2] = "DISCONNECTED";
-})(State || (State = {}));
