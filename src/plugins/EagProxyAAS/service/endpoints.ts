3317478781d7faa22f404083280706e20b2db838
q13x <84165981+WorldEditAxe@users.noreply.github.com>
2025-03-12 06:43:43 -0700

    added new eaglerpass endpoint

--- src/plugins/EagProxyAAS/service/endpoints.ts
+++ src/plugins/EagProxyAAS/service/endpoints.ts
@@ -1,10 +1,76 @@
 import { ServerDeviceCodeResponse, auth } from "../auth.js";
 import { config } from "../config.js";
 
+const LINK_STR = `
+<!doctype html>
+<html>
+  <head>
+    <title>EagPAAS - Connect</title>
+    <style>
+      :root {
+        font-family: "Arial";
+      }
+      code {
+        padding: 3px 10px 3px 10px;
+        border-radius: 5px;
+        font-family: monospace;
+        background-color: #1a1a1a;
+        color: white;
+        white-space: pre-wrap;
+      }
+    </style>
+    <script type="text/javascript">
+      window.addEventListener("load", () => {
+      let param = new URLSearchParams(window.location.search),
+      	url = param.get("url")
+      if (url == null) url = "ERROR!"
+      else {
+      	try {
+        const parsed = new URL(url), session = JSON.parse(parsed.searchParams.get("session"))
+        if (session.expires_on < Date.now()) {
+        	url = "EXPIRED: get a new link!"
+        }
+        } catch (_e) {
+        	console.error(_e)
+        }
+      }
+      
+        document.getElementById("connect-url").innerHTML =
+          url.replace(
+            "wss:",
+            window.location.protocol == "wss:" ? "wss:" : "ws:",
+          )
+       const parsedURL = new URL(url)
+        document.getElementById("connect-url-vanilla").innerHTML =
+         	"vanilla+online://" + parsedURL.searchParams.get("ip") + ":" + parsedURL.searchParams.get("port") + "/?session=" + parsedURL.searchParams.get("session")
+      })
+    </script>
+  </head>
+  <body>
+    <h1>EagPAAS - Connect to Server</h1>
+    <p>
+      Hello there! To connect to this Minecraft server through a server list entry,
+      use this URL: (connect from any recent
+      EaglercraftX client via Multiplayer > Direct Connect)
+      <br><br><code id="connect-url">loading...</code><br><br>
+      
+      If your Eaglercraft client supports <code>vanilla://</code> URLs, this will also work:
+      <br><br><code id="connect-url-vanilla">loading...</code><br><br>
+      
+      Note that your session will eventually expire, and you will need to log back in again.
+    </p>
+  </body>
+</html>
+
+`;
+
 export async function registerEndpoints() {
   const proxy = PLUGIN_MANAGER.proxy;
   proxy.on("httpConnection", (req, res, ctx) => {
-    if (req.url.startsWith("/eagpaas/metadata")) {
+    if (req.url.startsWith("/eagpaas/link")) {
+      ctx.handled = true;
+      res.setHeader("Content-Type", "text/html").writeHead(200).end(LINK_STR);
+    } else if (req.url.startsWith("/eagpaas/metadata")) {
       ctx.handled = true;
       res.writeHead(200).end(
         JSON.stringify({
